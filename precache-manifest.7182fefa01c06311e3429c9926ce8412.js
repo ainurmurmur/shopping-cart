f9689f25646fc42af97adfb1720b64c2bb631085
ainurmurmur <sagandykovaainur@gmail.com>
2020-11-06 16:08:41 +0600

    Updates

---
+++ precache-manifest.7182fefa01c06311e3429c9926ce8412.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4cefd201f9f714f0b573f088bfc82ec",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "02896d70e58019b584ca",
+    "url": "/shopping-cart/static/css/main.f1932d81.chunk.css"
+  },
+  {
+    "revision": "c20f46ea4f2bb8eb1201",
+    "url": "/shopping-cart/static/js/2.c5090910.chunk.js"
+  },
+  {
+    "revision": "232aae28f8ee03096b30a3e0fbca302d",
+    "url": "/shopping-cart/static/js/2.c5090910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02896d70e58019b584ca",
+    "url": "/shopping-cart/static/js/main.6b88de3f.chunk.js"
+  },
+  {
+    "revision": "4e804a8365c236b16721",
+    "url": "/shopping-cart/static/js/runtime-main.6c701d04.js"
+  },
+  {
+    "revision": "0504ecab325de67470cd0bbf93ed0f82",
+    "url": "/shopping-cart/static/media/donuts1back.0504ecab.png"
+  },
+  {
+    "revision": "ec72452b042c1facea960ae1176a94b3",
+    "url": "/shopping-cart/static/media/donuts2back.ec72452b.png"
+  }
+]);
